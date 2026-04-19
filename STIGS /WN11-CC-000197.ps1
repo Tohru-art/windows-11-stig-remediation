beabfdbeb35bdac774683e5d982b61663ad69284
Will | Bonk <65633219+Tohru-art@users.noreply.github.com>
2026-04-19 01:02:18 -0400

    Add PowerShell script to disable Windows consumer features
    This script modifies the Windows registry to disable consumer experiences on Windows 11.

---
+++ STIGS /WN11-CC-000197.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that Microsoft consumer experiences are turned off.
+
+.NOTES
+    Author          : Will-Garlens Pierre
+    LinkedIn        : linkedin.com/in/will-garlens-pierre/
+    GitHub          : github.com/Tohru-art
+    Date Created    : 2026-04-19
+    Last Modified   : 2026-04-19
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000197
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000197/
+
+.TESTED ON
+    Date(s) Tested  : 2026-04-19
+    Tested By       : Will-Garlens Pierre
+    Systems Tested  : Windows 11 Pro 25H2
+    PowerShell Ver. : 5.1
+
+.USAGE
+    Run this script as Administrator on the target Windows 11 system.
+    Example syntax:
+    PS C:\> .\WN11-CC-000197.ps1
+#>
+
+$path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+$name = "DisableWindowsConsumerFeatures"
+$value = 1
+
+if (-not (Test-Path $path)) {
+    New-Item -Path $path -Force | Out-Null
+}
+Set-ItemProperty -Path $path -Name $name -Value $value -Type DWord
+
+Write-Host "WN11-CC-000197 remediation applied. $name set to $value."
